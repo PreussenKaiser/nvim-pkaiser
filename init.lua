46235952d810b409d70dc54bbb3adce0bf212956
Karl Lukan <klukan20@gmail.com>
2023-12-08 23:46:05 -0600

    updating again since im using linux rn

--- init.lua
+++ init.lua
@@ -1,13 +1,19 @@
-require'lspconfig'.lua_ls.setup {
-    settings = {
-	Lua = {
-	    diagnostics = {
-		globals = { 'vim' },
-	    },
-	},
-    },
-}
-
 vim.o.number = true
 vim.o.shiftwidth = 4
 vim.o.autoindent = true
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v3.x',
+	requires = {
+	    { 'neovim/nvim-lspconfig' },
+	    { 'hrsh7th/nvim-cmp' },
+	    { 'hrsh7th/cmp-nvim-lsp' },
+	    { 'L3MON4D3/LuaSnip' },
+	} 
+    }
+end)
