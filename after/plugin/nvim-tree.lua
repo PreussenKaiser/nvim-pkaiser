6ed6f22fc9f197be7b2b57d9eaa51064a310db22
Karl Lukan <lukan20@gmail.com>
2024-08-25 14:12:09 -0500

    stuff

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,7 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup {
+local nvim_tree = require "nvim-tree"
+
+nvim_tree.setup {
     renderer = {
 	icons = {
 	    show = {
--- lua/settings.lua
+++ lua/settings.lua
@@ -2,4 +2,4 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
 vim.o.autoindent = true
-
+vim.opt.clipboard  = "unnamedplus"
