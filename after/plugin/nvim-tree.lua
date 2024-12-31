4c962c90416e073ce957a2a2047f8c532153c485
PreussenKaiser <preukaiser@proton.me>
2024-12-31 19:45:40 +0000

    added keybinds, enabled netrw

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,6 +1,3 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 local config = require 'nvim-tree'
 
 config.setup {
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,4 @@ local builtin = require 'telescope.builtin'
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { })
+vim.keymap.set('n', '<leader>rr', builtin.lsp_references, { })
